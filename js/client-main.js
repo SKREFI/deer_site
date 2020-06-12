6bfb82c00e7daaec409fee8a2331d9bb56f0d524
Razvan Sandu <srazvan43@yahoo.com>
2020-06-12 20:24:34 +0300

    Update client-main.js

--- js/client-main.js
+++ js/client-main.js
@@ -3,7 +3,8 @@ console.log("Deer's up!");
 //geting the firebase user from the local storage !!! NOT WORKING !! :(
 
 // ----- CONSTANTS -----
-const BASE_URL = 'http://localhost:5000'; // NodeJS API link
+const BASE_URL = 'http://localhost:5000' // NodeJS API link
+const BASE_URL = 'bc37039ee998.ngrok.io/' // For Ngrok app
 
 // ----- HTML Views -----
 const form = document.getElementById('post_form');
